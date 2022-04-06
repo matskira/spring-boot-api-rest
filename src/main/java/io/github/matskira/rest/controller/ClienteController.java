e392c1a653e6d0f99995a8ac4b34fba2c5088212
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-06 09:41:25 -0300

    Criado primeiro cliente controller

--- src/main/java/io/github/matskira/rest/controller/ClienteController.java
+++ src/main/java/io/github/matskira/rest/controller/ClienteController.java
@@ -1,5 +1,18 @@
 package io.github.matskira.rest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
+	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+	@ResponseBody
+	public String helloCliente(@PathVariable(name = "nome") String nomeCliente) {
+		return String.format("Hello "+nomeCliente);
+	}
 }
