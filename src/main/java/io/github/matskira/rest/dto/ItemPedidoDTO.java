799bdc03d5eb8b8a371591c6bc38081d0aaeaa89
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-07 11:38:08 -0300

    Criado classes de DTO e serviço

---
+++ src/main/java/io/github/matskira/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,24 @@
+package io.github.matskira.rest.dto;
+
+public class ItemPedidoDTO {
+
+	private Integer codProduto;
+	private Integer quantidade;
+
+	public Integer getCodProduto() {
+		return codProduto;
+	}
+
+	public void setCodProduto(Integer codProduto) {
+		this.codProduto = codProduto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
---
+++ src/main/java/io/github/matskira/rest/dto/PedidoDTO.java
@@ -0,0 +1,36 @@
+package io.github.matskira.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PedidoDTO {
+
+	private Integer idCliente;
+	private BigDecimal total;
+	private List<ItemPedidoDTO> itens;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public List<ItemPedidoDTO> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemPedidoDTO> itens) {
+		this.itens = itens;
+	}
+
+}
