e3baa87491c1ab1b289b3877e15f7ec057daff79
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-13 10:53:35 -0300

    Criado algoritmo de autenticação em memória com BCrypt

--- src/main/java/io/github/matskira/config/SecurityConfig.java
+++ src/main/java/io/github/matskira/config/SecurityConfig.java
@@ -1,18 +1,37 @@
 package io.github.matskira.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	/**
+	 * Método responsável por criptografar a senha seguindo padrão BCrypt
+	 * 
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	/**
+	 * Método responsável pelo algoritmo de autenticação
+	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("matheus")
+				.password(passwordEncoder().encode("123")).roles("USER");
 	}
-	
+
+	/**
+	 * Método responsável pelo algoritmo de autorizar pós autenticação
+	 */
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		super.configure(http);
